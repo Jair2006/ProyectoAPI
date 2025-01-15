186df661d08925c45db2d6ae6f8aa1b5e61a0131
Jair <unifly.jair@gmail.com>
2025-01-14 19:24:47 -0500

    controlado agregado para busqueda por id y busqueda dinamica

--- src/controllers/item.controller.js
+++ src/controllers/item.controller.js
@@ -1,38 +1,64 @@
 import Item from "../models/item.model.js";
+import { Op } from "sequelize";
 let id = 0;
 
 export const getItems = async () => {
-    let data = await Item.findAll();
-    return data;
-}
+  let data = await Item.findAll();
+  return data;
+};
 
-export const getItemById = (req, res) => {
-    const params = req.params; // {id: 10}
-    res.send(`Obteniendo el item con ID ${params?.id}`);
-}
+export const getItemById = async (id) => {
+  let data = await Item.findByPk(id);
+  return data;
+};
 
-export const getItemsBySearch = (req, res) => {
-    const queryParams = req.query;
-    res.send(`Obteniendo item que cumplan con la búsqueda: ${JSON.stringify(queryParams)}`);
-}
+export const getItemsBySearch = async (filters) => {
+  let whereFilters = {};
+
+  if (filters.name) {
+    Object.assign(whereFilters, {
+      name: {
+        [Op.iLike]: `%${filters.name}%`,
+      },
+    });
+  }
+  if (filters.category) {
+    Object.assign(whereFilters, { category: filters.category });
+  }
+  if (filters.maxPrice || filters.minPrice) {
+    Object.assign(whereFilters, {
+      price: {
+        [Op.lte]: filters.maxPrice ?? 9999999,
+        [Op.gte]: filters.minPrice ?? 0
+      },
+    });
+  }
+
+  let data = await Item.findAll({
+    where: {
+      [Op.and]: whereFilters,
+    },
+  });
+  return data;
+};
 
 export const createItem = async (body) => {
-    let data = await Item.create(body);
-    return data;
-}
+  let data = await Item.create(body);
+  return data;
+};
 
 export const updateItem = async (id, body) => {
-    let data = await Item.update(body, {
-        where: {
-            id: id
-        }
-    });
-    // let item = await Item.findByPk(id);
-    // let data = await item.update("name", body.name);
-    return data[0];
-}
+  let data = await Item.update(body, {
+    where: {
+      id: id,
+    },
+  });
+  // let item = await Item.findByPk(id);
+  // let data = await item.update("name", body.name);
+  return data[0];
+};
 
 export const deleteItem = (req, res) => {
-    const { id } = req.params;
-    res.send(`Eliminando el item con id: ${id}`);
-}
+  const { id } = req.params;
+  res.send(`Eliminando el item con id: ${id}`);
+};
--- src/routes/item.router.js
+++ src/routes/item.router.js
@@ -1,42 +1,82 @@
 import express from "express";
-import { getItemById, getItemsBySearch, getItems, createItem, updateItem, deleteItem } from '../controllers/item.controller.js';
+import {
+  getItemById,
+  getItemsBySearch,
+  getItems,
+  createItem,
+  updateItem,
+  deleteItem,
+} from "../controllers/item.controller.js";
 
 const itemRouter = express.Router();
 
-itemRouter.get('/search', getItemsBySearch);
-itemRouter.get('/:id', getItemById);
 
-itemRouter.get('/', (req, res) => {
-    getItems().then((data) => {
-        res.status(200).json(data);
-    }).catch((err) => {
-        console.error("Error on GET / route:", err);
-        res.status(500).json({message: err});
-    });
-});
 
-itemRouter.post('/', (req, res) => {
-    createItem(req.body).then((data) => {
-        res.status(201).json(data);
-    }).catch((err) => {
-        console.error("Error on POST / route:", err);
-        res.status(500).json({message: err});
-    });
-});
-
-itemRouter.put('/:id', (req, res) => {
-    updateItem(req.params.id, req.body).then((data) => {
-        if (data) {
-            res.status(201).json({message: "Item updated.", data: data});
-        } else {
-            res.status(400).json({message: "Item not updated.", data: data});
+itemRouter.get("/search", (req, res)=>{
+    getItemsBySearch(req.query).then((data)=> {
+        if(data.length){
+            res.status(200).json(data)
+        }else{
+            res.status(404).json({message: "Item not found"})
         }
-    }).catch((err) => {
-        console.error("Error on PUT / route:", err);
-        res.status(500).json({message: err});
+    }).catch((err)=>{
+        console.error("Error on GET /search route:", err);
+        res.status(500).json({message:err})
+    })
+});
+
+itemRouter.get("/:id", (req, res) => {
+  getItemById(req.params.id)
+    .then((data) => {
+      if (data) {
+        res.status(200).json(data);
+      } else {
+        res.status(400).json({ message: "Item not found" });
+      }
+    })
+    .catch((err) => {
+      console.error("Error on Get /:id route", err);
+      res.status(404).json({ message: err });
     });
 });
 
-itemRouter.delete('/:id', deleteItem);
+itemRouter.get("/", (req, res) => {
+  getItems()
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      console.error("Error on GET / route:", err);
+      res.status(500).json({ message: err });
+    });
+});
+
+itemRouter.post("/", (req, res) => {
+  createItem(req.body)
+    .then((data) => {
+      res.status(201).json(data);
+    })
+    .catch((err) => {
+      console.error("Error on POST / route:", err);
+      res.status(500).json({ message: err });
+    });
+});
+
+itemRouter.put("/:id", (req, res) => {
+  updateItem(req.params.id, req.body)
+    .then((data) => {
+      if (data) {
+        res.status(201).json({ message: "Item updated.", data: data });
+      } else {
+        res.status(400).json({ message: "Item not updated.", data: data });
+      }
+    })
+    .catch((err) => {
+      console.error("Error on PUT / route:", err);
+      res.status(500).json({ message: err });
+    });
+});
+
+itemRouter.delete("/:id", deleteItem);
 
 export default itemRouter;
